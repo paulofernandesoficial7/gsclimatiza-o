15e99d31078a0a68a3ad724f3b27ddd1a5ea223c
paulofernandesoficial7 <paulofernandesoficial7@gmail.com>
2026-06-16 19:55:08 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
-const fs = require('fs');
+const path = require('path');
 
-// O servidor lê o arquivo db.json
+app.use(express.json());
+// Isso serve seus arquivos (index.html, app.js, style.css) automaticamente
+app.use(express.static(__dirname));
+
+// Rota de exemplo para listar dados do db.json
 app.get('/api/dados', (req, res) => {
+    const fs = require('fs');
     const data = JSON.parse(fs.readFileSync('./db.json', 'utf8'));
     res.json(data);
 });
